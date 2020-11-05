b64449c0d665e198b60e293a2c9414d2938656d0
Vladislav Ivanov <himametsu@gmail.com>
2020-11-05 16:15:28 +0300

    feature: added a test version of the stack

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from 'react';
 import { inject, observer } from 'mobx-react';
 import './Input.scss';
 
+const operators = ['+', '-', '*', '/', '='];
+
 const Input = inject('menuStore')(observer(({ menuStore }) => {
   const refInput = useRef();
 
@@ -17,6 +19,18 @@ const Input = inject('menuStore')(observer(({ menuStore }) => {
     <div className='input'>
       <div className='input__stack'>
         {menuStore.stack}
+      </div>
+      <div className='input__stack'>
+        {menuStore.testStack.map((item) => {
+          return (
+            <div className='stack__item'>
+              {operators.includes(item)
+                ? item === '/' ? '÷' : item
+                : Number(item)
+              }
+            </div>
+          );
+        })}
       </div>
       <div className='input__enter'>
         <input
