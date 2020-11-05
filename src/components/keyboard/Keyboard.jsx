042039f14768f381024abf4d5f01c0d4c8dfc146
Vladislav Ivanov <himametsu@gmail.com>
2020-11-05 16:23:58 +0300

    feature: implemented a keyboard

---
+++ src/components/keyboard/Keyboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import { data } from './data';
+import Key from './Key';
+import './Keyboard.scss';
+
+const Keyboard = inject('menuStore')(observer(({ menuStore }) => {
+  console.log(menuStore);
+  return (
+    <div className='keyboard'>
+      {data.map((row, index) => {
+        return (
+          <div className='row' key={index}>
+            {row.map((btn) => (
+              <Key {...btn} store={menuStore} />
+            ))}
+          </div>
+        )
+      })}
+    </div>
+  );
+}));
+
+Keyboard.displayName = "Keyboard";
+export default Keyboard;
