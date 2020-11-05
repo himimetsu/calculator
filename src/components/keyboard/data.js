b1b37503da6858c510217ec08a60722804fc5b72
Vladislav Ivanov <himametsu@gmail.com>
2020-11-05 16:05:42 +0300

    feature: created a database of keys

---
+++ src/components/keyboard/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  [
+    {
+      text: 'CE',
+      key: 'clearEntry',
+      type: 'clearEntry',
+    },
+    {
+      text: 'C',
+      key: 'clear',
+      type: 'clear',
+    },
+    {
+      text: '⇦',
+      key: 'delete',
+      type: 'delete',
+    },
+    {
+      text: '÷',
+      key: 'divide',
+      type: 'divide',
+    },
+  ],
+  [
+    {
+      text: '7',
+      key: 'digit7',
+      type: 'digit',
+    },
+    {
+      text: '8',
+      key: 'digit8',
+      type: 'digit',
+    },
+    {
+      text: '9',
+      key: 'digit9',
+      type: 'digit',
+    },
+    {
+      text: '×',
+      key: 'multiply',
+      type: 'multiply',
+    },
+  ],
+  [
+    {
+      text: '4',
+      key: 'digit4',
+      type: 'digit',
+    },
+    {
+      text: '5',
+      key: 'digit5',
+      type: 'digit',
+    },
+    {
+      text: '6',
+      key: 'digit6',
+      type: 'digit',
+    },
+    {
+      text: '-',
+      key: 'minus',
+      type: 'minus',
+    },
+  ],
+  [
+    {
+      text: '1',
+      key: 'digit1',
+      type: 'digit',
+    },
+    {
+      text: '2',
+      key: 'digit2',
+      type: 'digit',
+    },
+    {
+      text: '3',
+      key: 'digit3',
+      type: 'digit',
+    },
+    {
+      text: '+',
+      key: 'plus',
+      type: 'plus',
+    },
+  ],
+  [
+    {
+      text: '+/-',
+      key: 'denial',
+      type: 'denial',
+    },
+    {
+      text: '0',
+      key: 'digit0',
+      type: 'digit',
+    },
+    {
+      text: ',',
+      key: 'comma',
+      type: 'comma',
+    },
+    {
+      text: '=',
+      key: 'equally',
+      type: 'equally',
+    },
+  ],
+];
