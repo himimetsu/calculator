f7822fe69884c03af937f6fc2f5b0fbaa7153bad
Vladislav Ivanov <himametsu@gmail.com>
2020-11-06 14:34:02 +0300

    feature: added a method for work with float numbers

--- src/store/index.js
+++ src/store/index.js
@@ -23,8 +23,56 @@ class MenuStore {
   @action setIsKeyboard() {
     this.isKeyboard = !this.isKeyboard;
   };
+
+  fixedValue(a, b, callOperator) {
+    if (typeof a === 'number' || typeof b === 'number') {
+      a = String(Number(a));
+      b = String(Number(b));
+    }
+
+    let value = 0;
+    const aCoord = a.indexOf('.');
+    const bCoord = b.indexOf('.');
+
+    switch (callOperator) {
+      case '+':
+      case '-':
+        value = Math.max(a.slice(aCoord + 1).length, b.slice(bCoord + 1).length);
+        break;
+      case '*':
+        {
+          if (aCoord > -1) {
+            value += a.slice(aCoord + 1).length;
+          }
+
+          if (bCoord > -1) {
+            value += b.slice(bCoord + 1).length;
+          }
+        }
+        break;
+      case '/':
+        value = 0;
+        break;
+    }
+
+    let countZero = 0;
+
+    const maxAmountZero = Math.max(a.slice(aCoord + 1).length, b.slice(bCoord + 1).length);
+
+    a = a * Math.pow(10, maxAmountZero);
+    b = b * Math.pow(10, maxAmountZero);
+
+    if (callOperator === '/') {
+      countZero = Math.pow(10, value);
+    } else {
+      countZero = Math.pow(10, maxAmountZero);
+    }
+
+    return [a, b, countZero];
+  };
+
   startCalc(callOperator) {
-    this.testStack.push(this.value, callOperator); //
+    this.testStack.push(this.value, callOperator);
     this.stack = this.value + `${callOperator}`;
     this.firstValue = this.value;
     this.value = '0';
